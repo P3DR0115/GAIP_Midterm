568f3484c580e2841b4f21bc939ed897d338d19c
Pedro A. Tresgallo <P3dro115@gmail.com>
2019-03-18 15:15:51 -0500

    Add Camera Movement For Player
    Add Camera Movement, Still in the works

---
+++ Underworld/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    GameObject Parent;
+    Vector2 cameraDir;
+    Vector3 RelativePlayer;
+    public float HSpeed, VSpeed;
+    private float Yaw, YawAxis, YawAxisPrev, Pitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HSpeed = VSpeed = 2.0f;
+
+        if(Parent == null)
+        {
+            GameObject[] candidates = FindObjectsOfType<GameObject>();
+
+            foreach(GameObject go in candidates)
+            {
+                if (go.tag == "Player")
+                    Parent = go;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetAxis("Mouse X") != YawAxisPrev)
+        {
+            YawAxis = Input.GetAxis("Mouse X");
+            Yaw += HSpeed * Input.GetAxis("Mouse X");
+
+        }
+
+        YawAxisPrev = YawAxis;
+
+        //cameraDir = new Vector2(Yaw, Pitch);
+        //cameraDir.Normalize();
+        RelativePlayer = Parent.transform.position;
+        RelativePlayer.y = 0;
+        
+        transform.RotateAround(Parent.transform.position, Vector3.up, Yaw);
+        transform.rotation.SetLookRotation(RelativePlayer);
+        //transform.eulerAngles = new Vector3(Pitch, 0.0f, 0.0f);
+
+    }
+}
---
+++ Underworld/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody RB;
+    public Vector3 Direction; // checking to see if any keys are being pressed for movement
+    
+    public float MoveHorizontal, MoveVertical, Speed;
+    public bool RestrainY, RestrainX;
+    public bool IsKeyDown   //Boolean the lets player know is a key is down useful to stop player if no keys are pressed
+    {
+        get
+        {
+            if (Direction.sqrMagnitude == 0) return false;
+            return true;
+        }
+    }
+
+    void Awake()
+    {
+        RB = GetComponent<Rigidbody>();
+        this.Speed = 15f;
+        this.Direction = Vector3.zero;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.Direction.x = this.Direction.z = 0;
+        //rb2D.drag = 0;
+
+        MoveHorizontal = Input.GetAxis("Horizontal");
+        MoveVertical = Input.GetAxis("Vertical");
+
+        // x is lateral, y is vertical, z is forward/backward
+        this.Direction = new Vector3(MoveHorizontal, 0, MoveVertical);
+        this.Direction.Normalize();
+        RB.AddForce(Direction * Speed);
+
+        //this.transform.Translate(Direction.x * Speed, Direction.z * Speed, 0);
+
+        if ((Direction.x < 1 || Direction.x > -1)
+            && (Direction.z < 1 || Direction.z > -1))
+        {
+            //rb2D.velocity.Scale(new Vector2(0, 0));// .Set(0, 0);
+            //rb2D.velocity.Set(0, 0);
+            //rb2D.
+            //rb2D.drag = 100000;
+
+        }
+
+        //Get Keys From Input Directly
+        Direction.x = Direction.y = 0;
+        //direction += direction;
+        Direction.Normalize();  //Normalize
+        
+
+    }
+}
