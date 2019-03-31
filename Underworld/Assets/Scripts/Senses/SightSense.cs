bae6ce25fb88ec11a7f74dc7f32fc7c12ed02292
Pedro A. Tresgallo <P3dro115@gmail.com>
2019-03-31 12:55:31 -0500

    AI Behaviors
    Add Patrol State
    Refined Chase State

--- Underworld/Assets/Scripts/Senses/SightSense.cs
+++ Underworld/Assets/Scripts/Senses/SightSense.cs
@@ -3,28 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum BehaviorState
+{
+    Idle,
+    Patrol,
+    Chase,
+    Investigate,
+
+}
+
 public class SightSense : MonoBehaviour
 {
     NavMeshAgent agent;
     public GameObject Player;
     public Transform lastKnownPlayerPosition;
-    //public BoxCollider VisionBox;
-    public bool chase; // used to currently chase.
-    public bool chasedBefore; // used to determine last known position.
-    float chaseTimeoutTimer; // how long the chase is in effect (e.g. chase for 5 seconds)
+    public Vector3 PatrolPoint;
+    public int maxPatrolRange; // Range for the random number
+    public float patrolTimeoutDuration;
+    public float patrolUntil;
+
+    public BehaviorState currentState;
+    public float chaseTimeoutDuration; // how long the chase is in effect (e.g. chase for 5 seconds)
     public float chaseUntil; // Once the game time exceeds the chaseTimeoutTimer, will stop chasing.
 
     public void Awake()
     {
-        chase = false;
-        chasedBefore = false;
-        chaseTimeoutTimer = 2.8f;
+        currentState = BehaviorState.Idle;
+        patrolTimeoutDuration = 4.0f;
+        chaseTimeoutDuration = 2.5f;
+        maxPatrolRange = 10;
         agent = GetComponentInParent<NavMeshAgent>();
         lastKnownPlayerPosition = this.transform; // initially doesn't know where player is.
-        //VisionBox = GetComponentInChildren<BoxCollider>();
+
+        FindPlayer();
+    }
+
+    private void FindPlayer()
+    {
         GameObject[] temp = FindObjectsOfType<GameObject>();
 
-        foreach(GameObject GO in temp)
+        foreach (GameObject GO in temp)
         {
             if (GO.tag == "Player")
             {
@@ -42,33 +60,99 @@ public class SightSense : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CheckChaseTime();
-
-        ChaseDecision();
+        BehaviorDecision();
 
     }
 
-    private void ChaseDecision()
+    private void BehaviorDecision()
     {
-        if (chase)
+        switch(currentState)
         {
-            chasedBefore = true;
-            agent.destination = Player.transform.position;
+            case BehaviorState.Idle:
+                {
+                    GeneratePatrolPoint();
+                    patrolUntil = Time.time + patrolTimeoutDuration;
+                    currentState = BehaviorState.Patrol;
+                    break;
+                }
+            case BehaviorState.Chase:
+                {
+                    agent.destination = Player.transform.position;
+                    CheckChaseTime();
+                    break;
+                }
+            case BehaviorState.Investigate:
+                {
+                    agent.destination = lastKnownPlayerPosition.position;
+
+                    currentState = BehaviorState.Patrol;
+                    break;
+                }
+            case BehaviorState.Patrol:
+                {
+                    agent.destination = PatrolPoint;
+
+                    if (Time.time > patrolUntil || (Mathf.Approximately(this.transform.position.x, PatrolPoint.x) && Mathf.Approximately(this.transform.position.z, PatrolPoint.z)))
+                    {
+                        currentState = BehaviorState.Idle;
+                    }
+
+                    break;
+                }
         }
-        else
-        {
-            agent.destination = lastKnownPlayerPosition.position;
-        }
+        
     }
 
     private void CheckChaseTime()
     {
-        if (Time.deltaTime > chaseUntil)
+        if (Time.time > chaseUntil)
         {
-            chase = false;
+            lastKnownPlayerPosition = Player.transform;
+            currentState = BehaviorState.Investigate;
 
-            if (chasedBefore)
-                lastKnownPlayerPosition = Player.transform;
+            //if (chasedBefore)
+            //    lastKnownPlayerPosition = Player.transform;
+        }
+    }
+
+    private void GeneratePatrolPoint()
+    {
+        PatrolPoint = new Vector3();
+
+        System.Random j, k;
+        j = new System.Random();
+        k = new System.Random();
+
+        PatrolPoint.x = j.Next(maxPatrolRange);
+        PatrolPoint.z = k.Next(maxPatrolRange);
+
+        // Switch to determine which quadrant the movement will be towards.
+        switch(j.Next(4))
+        {
+            case 0:
+                {
+                    // Both Positive
+                    break;
+                }
+            case 1:
+                {
+                    // x negative
+                    PatrolPoint.x *= -1;
+                    break;
+                }
+            case 2:
+                {
+                    // z negative
+                    PatrolPoint.z *= -1;
+                    break;
+                }
+            case 3:
+                {
+                    // Both Negative
+                    PatrolPoint.x *= -1;
+                    PatrolPoint.z *= -1;
+                    break;
+                }
         }
     }
 
@@ -76,8 +160,29 @@ public class SightSense : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            chase = true;
-            chaseUntil = Time.deltaTime + chaseTimeoutTimer;
+            currentState = BehaviorState.Chase;
+            chaseUntil = Time.time + chaseTimeoutDuration;
+
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            currentState = BehaviorState.Chase;
+            chaseUntil = Time.time + chaseTimeoutDuration;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            lastKnownPlayerPosition = Player.transform;
+            //currentState = BehaviorState.Investigate;
 
         }
     }
